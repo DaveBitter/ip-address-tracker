1f0766b9354e8e7b7705e4ff4b08880aa27b24fe
Dave Bitter <dbitter@anwb.nl>
2021-12-15 11:42:26 +0100

    refactor(app): Fix TS errors and simplify logic

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react';
 import { ActionFunction, Form, LoaderFunction, Outlet, useActionData, useLoaderData } from 'remix';
 
+/*** Asset links ***/
 export const links = () => [
   {
     rel: "preload",
@@ -22,52 +23,31 @@ export const links = () => [
   }
 ];
 
-function validateIP(IP: string) {
-  if (IP.length < 2) {
-    return `That joke's name is too short`;
-  }
-}
-
+/*** Action handler ***/
 type ActionData = {
-  data?: any;
-  formError?: string;
-  fieldErrors?: {
-    name: string | undefined;
-    content?: string | undefined;
+  location?: {
+    country: string;
+    region: string;
+    timezone: string;
   };
-  fields?: {
-    IP: string;
-  };
+  ip?: string;
+  isp?: string;
 };
 
 export const action: ActionFunction = async ({
   request
 }): Promise<Response | ActionData> => {
-
   const form = await request.formData();
 
-  const IP = form.get("ip");
-
-  if (
-    typeof IP !== "string"
-  ) {
-    return { formError: `Form not submitted correctly.` };
-  }
-
-  const fieldErrors = {
-    name: validateIP(IP)
-  };
-  const fields = { IP };
-  if (Object.values(fieldErrors).some(Boolean)) {
-    return { fieldErrors, fields };
-  }
+  const ip = form.get("ip") as string;
 
   const GEO_IP_API_KEY = 'at_lhknRFKpkdlDUiZmAA7Zmt2EGsvu2';
-  const data = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${GEO_IP_API_KEY}&ipAddress=${IP}`).then(res => res.json())
+  const { location, isp } = await fetch(`https://geo.ipify.org/api/v2/country?apiKey=${GEO_IP_API_KEY}&ipAddress=${ip}`).then(res => res.json())
 
-  return { data, fields: { IP } };
+  return { location, isp, ip };
 };
 
+/*** Loader handler ***/
 type LoaderData = {
   userIP?: string;
 };
@@ -79,48 +59,55 @@ export const loader: LoaderFunction = async ({ }) => {
 };
 
 let ReactLeaflet: any;
+let LeafletGeoSearch: any;
+let hasAutoSubmitted = false;
 
-export default function Index() {
-  const [hasWindowAndLeaflet, setHasWindowAndLeaflet] = useState(false);
-  const actionData = useActionData<
-    ActionData | undefined
-  >();
-
+/*** Component ***/
+const Index = () => {
+  const actionData = useActionData<ActionData | undefined>();
   const loaderData = useLoaderData<LoaderData>();
 
-  const ipAddress = actionData?.data?.IP || loaderData?.userIP || ''
+  const ipAddress = actionData?.ip || loaderData?.userIP || ''
 
+  /*** Leaflet map ***/
+  const ipInputRef = useRef<null | HTMLInputElement>(null);
+  const formRef = useRef<null | HTMLFormElement>(null);
 
-  const ipInputRef = useRef();
-  const formRef = useRef();
-  useEffect(() => {
-    import('react-leaflet').then(reactLeaflet => {
-      ReactLeaflet = reactLeaflet;
-      setHasWindowAndLeaflet(true)
-      // @ts-ignore
-      !actionData?.data && ipInputRef?.current?.value && formRef?.current && formRef?.current?.submit();
-    })
-  }, [])
-
-  const [map, setMap] = useState()
+  const [hasWindowAndLeaflet, setHasWindowAndLeaflet] = useState(false);
+  const [map, setMap] = useState<null | { setView: (latLng: [number, number], zoom: number) => void }>(null)
   const [position, setPosition] = useState([52.3139713, 4.9419641]);
 
   useEffect(() => {
-    if (!actionData?.data?.location?.country) { return; }
+    async function loadLeaflet() {
+      if (!ReactLeaflet) { ReactLeaflet = await import('react-leaflet') }
+      setHasWindowAndLeaflet(true) // NOTE: added to force rerender and load the map in the DOM
+    }
 
-    import('leaflet-geosearch').then(async (leafletGeosearch: any) => {
-      const provider = new leafletGeosearch.OpenStreetMapProvider();
-      const [locationData] = await provider.search({ query: `${actionData.data.location.country} ${actionData.data.location.region}` });
+    async function fetchLocationData() {
+      if (!LeafletGeoSearch) { LeafletGeoSearch = await import('leaflet-geosearch') }
 
-      // @ts-ignore
+      const provider = new LeafletGeoSearch.OpenStreetMapProvider();
+      const [locationData] = await provider.search({ query: `${actionData?.location?.country} ${actionData?.location?.region}` });
+
       if (locationData && map?.setView) {
-        // @ts-ignore
         map.setView([locationData.y, locationData.x], 10)
 
         setPosition([locationData.y, locationData.x])
       }
-    })
-  }, [map])
+    }
+
+    function autoSubmitForm() {
+      hasAutoSubmitted = true;
+      formRef.current?.submit();
+    }
+
+    const shouldFetchLocationData = actionData?.location?.country;
+    const shouldAutoSubmitForm = !hasAutoSubmitted && !actionData?.location && ipInputRef?.current?.value && formRef?.current;
+
+    loadLeaflet();
+    shouldFetchLocationData && fetchLocationData();
+    shouldAutoSubmitForm && autoSubmitForm();
+  }, [map, actionData?.location])
 
   return (
     <div className='h-screen'>
@@ -130,24 +117,26 @@ export default function Index() {
           <Form className='w-full lg:w-96 relative' method='post' ref={formRef}>
             <label htmlFor="ip" className='sr-only'>IP address or domain</label>
             <input ref={ipInputRef} defaultValue={ipAddress} id="ip" name="ip" minLength={7} maxLength={15} size={15} pattern="^((\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$" className='w-full p-4 pr-14 rounded-2xl text-gray-700 text-xl font-medium' placeholder='Search for any IP address or domain'></input>
-            <button type='submit' className='w-14 h-full absolute top-0 right-0 rounded-r-2xl bg-black hover:bg-gray-800 transition-colors bg-[url("/img/icons/arrow.svg")] bg-no-repeat bg-center' />
+            <button type='submit' className='w-14 h-full absolute top-0 right-0 rounded-r-2xl bg-black hover:bg-gray-800 transition-colors bg-[url("/img/icons/arrow.svg")] bg-no-repeat bg-center'>
+              <span className='sr-only'>Retrieve IP information</span>
+            </button>
           </Form>
           <dl className='flex flex-col lg:flex-row justify-between items-stretch w-full lg:w-auto mt-4 lg:py-12 rounded-2xl bg-white shadow-lg'>
-            <div className='flex flex-col py-6 lg:py-0 px-12 items-center lg:border-solid lg:border-r-2 lg:border-gray-200'>
+            <div className='flex flex-col items-center max-w-md py-6 lg:py-0 px-12 lg:border-solid lg:border-r-2 lg:border-gray-200 text-center'>
               <dt className='text-gray-400 text-sm font-bold uppercase tracking-widest'>IP address</dt>
-              <dd className='text-gray-700 text-2xl font-bold'>{actionData?.fields?.IP || 'Unavailable'}</dd>
+              <dd className='text-gray-700 text-2xl font-bold'>{actionData?.ip || 'Unavailable'}</dd>
             </div>
-            <div className='flex flex-col py-6 lg:py-0 px-12 items-center lg:border-solid lg:border-r-2 lg:border-gray-200'>
+            <div className='flex flex-col items-center max-w-md py-6 lg:py-0 px-12 lg:border-solid lg:border-r-2 lg:border-gray-200 text-center'>
               <dt className='text-gray-400 text-sm font-bold uppercase tracking-widest'>Location</dt>
-              <dd className='text-gray-700 text-2xl font-bold'>{actionData?.data?.location?.country || 'Unavailable'}</dd>
+              <dd className='text-gray-700 text-2xl font-bold'>{actionData?.location?.country || 'Unavailable'}</dd>
             </div>
-            <div className='flex flex-col py-6 lg:py-0 px-12 items-center lg:border-solid lg:border-r-2 lg:border-gray-200'>
+            <div className='flex flex-col items-center max-w-md py-6 lg:py-0 px-12 lg:border-solid lg:border-r-2 lg:border-gray-200 text-center'>
               <dt className='text-gray-400 text-sm font-bold uppercase tracking-widest'>Timezone</dt>
-              <dd className='text-gray-700 text-2xl font-bold'>{actionData?.data?.location?.timezone || 'Unavailable'}</dd>
+              <dd className='text-gray-700 text-2xl font-bold'>{actionData?.location?.timezone || 'Unavailable'}</dd>
             </div>
-            <div className='flex flex-col py-6 lg:py-0 px-12 items-center'>
+            <div className='flex flex-col items-center max-w-md py-6 lg:py-0 px-12 text-center'>
               <dt className='text-gray-400 text-sm font-bold uppercase tracking-widest'>ISP</dt>
-              <dd className='text-gray-700 text-2xl font-bold'>{actionData?.data?.isp || 'Unavailable'}</dd>
+              <dd className='text-gray-700 text-2xl font-bold'>{actionData?.isp || 'Unavailable'}</dd>
             </div>
           </dl>
         </div>
@@ -155,14 +144,14 @@ export default function Index() {
       <main className='h-full lg:h-[calc(100%_-_16rem)]'>
         <section className='h-full'>
           <div id='map' className='relative flex justify-center items-center h-full bg-gray-200'>
-            <h1 className='fade-in absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center lg:whitespace-nowrap text-gray-700 text-2xl lg:text-6xl font-bold'>{actionData ? `${actionData.data.location.region}, ${actionData.data.location.country}` : ''}</h1>
+            <h1 className='fade-in absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center lg:whitespace-nowrap text-gray-700 text-2xl lg:text-6xl font-bold'>{actionData?.location ? `${actionData.location.region}, ${actionData.location.country}` : ''}</h1>
             {hasWindowAndLeaflet && <ReactLeaflet.MapContainer className='h-full w-full' scrollWheelZoom={false} whenCreated={setMap}>
               <ReactLeaflet.TileLayer
                 url='https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png'
               />
               <ReactLeaflet.Marker position={position}>
                 <ReactLeaflet.Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
+                  {actionData?.location ? `${actionData.location.region}, ${actionData.location.country}` : ''}
                 </ReactLeaflet.Popup>
               </ReactLeaflet.Marker>
             </ReactLeaflet.MapContainer>}
@@ -173,3 +162,5 @@ export default function Index() {
     </div >
   );
 }
+
+export default Index
